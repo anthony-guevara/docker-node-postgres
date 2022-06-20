f9b95223fb06502ae91996bf75b16066b2f80fa6
Anthony Guevara <yalidanthony@gmail.com>
2022-06-20 06:49:33 -0600

    Coneccion a MYSQL

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,14 +1,23 @@
-const { Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 const { config } = require('../config/config');
 const setUpModels = require('../db/models/index');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-    dialect: 'postgres',
-    logging:true
+  dialect: 'mysql',
+  logging: true,
+//   dialectOptions: {
+//     options: {
+//       enableArithAbort: false,
+//       cryptoCredentialsDetails: {
+//         minVersion: 'TLSv1',
+//       },
+//     },
+//   },
 });
 
 setUpModels(sequelize);
